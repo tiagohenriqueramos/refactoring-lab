035b39705f4ce037b19724792645b420cebc514d
tiagohenriqueramos <tiagohenriqueramos@hotmail.com>
2026-08-17 21:10:10 -0300

    commit inicial de configuração e instalação do projeto

---
+++ src/main/java/br/com/refactoringlab/RefactoringLabApplication.java
@@ -0,0 +1,13 @@
+package br.com.refactoringlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefactoringLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RefactoringLabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/refactoringlab/RefactoringLabApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.refactoringlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefactoringLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
