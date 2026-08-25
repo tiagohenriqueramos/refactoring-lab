16a0502062a315d4e46f8f5e99d3a24ab2ce7cc3
tiagohenriqueramos <tiagohenriqueramos@hotmail.com>
2026-08-25 17:22:43 -0300

    Teste unitario

--- src/test/java/br/com/refactoringlab/RefactoringLabApplicationTests.java
+++ src/test/java/br/com/refactoringlab/RefactoringLabApplicationTests.java
@@ -3,7 +3,11 @@ package br.com.refactoringlab;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+        "spring.docker.compose.enabled=false",
+        "spring.data.mongodb.uri=mongodb://localhost:27017/test",
+        "spring.data.mongodb.database=test"
+})
 class RefactoringLabApplicationTests {
 
     @Test
