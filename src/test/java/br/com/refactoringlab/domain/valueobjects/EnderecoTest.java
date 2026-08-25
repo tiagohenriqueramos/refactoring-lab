2c5fc586129eb5d619aa8d8ccf03e4d5d84b84a5
tiagohenriqueramos <tiagohenriqueramos@hotmail.com>
2026-08-25 14:07:03 -0300

    Teste unitario

--- src/test/java/br/com/refactoringlab/domain/valueobjects/EnderecoTest.java
+++ src/test/java/br/com/refactoringlab/domain/valueobjects/EnderecoTest.java
@@ -27,15 +27,6 @@ class EnderecoTest {
     }
 
     @Test
-    @DisplayName("Deve formatar endereço quando complemento for nulo")
-    void deveFormatarEnderecoComComplementoNulo() {
-        var endereco = new Endereco("Rua das Flores", "123", null, "Centro", "Sao Paulo", "SP", "01000-000");
-
-        assertThat(endereco.enderecoFormatado())
-                .isEqualTo("Rua das Flores, 123, Centro - Sao Paulo/SP, CEP: 01000-000");
-    }
-
-    @Test
     @DisplayName("Deve lançar exceção quando cidade for nula")
     void deveLancarExcecaoQuandoCidadeNula() {
         assertThatThrownBy(() -> new Endereco("Rua", "1", null, "Centro", null, "SP", "01000-000"))
@@ -49,6 +40,15 @@ class EnderecoTest {
         assertThatThrownBy(() -> new Endereco("Rua", "1", null, "Centro", "Sao Paulo", null, "01000-000"))
                 .isInstanceOf(NullPointerException.class)
                 .hasMessage("O estado (UF) é obrigatório");
+    }
+
+    @Test
+    @DisplayName("Deve formatar endereço quando complemento for nulo")
+    void deveFormatarEnderecoComComplementoNulo() {
+        var endereco = new Endereco("Rua das Flores", "123", null, "Centro", "Sao Paulo", "SP", "01000-000");
+
+        assertThat(endereco.enderecoFormatado())
+                .isEqualTo("Rua das Flores, 123, Centro - Sao Paulo/SP, CEP: 01000-000");
     }
 }
 
